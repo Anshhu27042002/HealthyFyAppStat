104dd7f3374e12d2aad5701253cb627b7f369977
Anshul Ghumadwar <anshulghumadwar04@gmail.com>
2023-04-08 22:10:31 +0530

    commit

---
+++ app/src/main/java/com/example/devan/remedaily/Models/NonDailyMedicine.java
@@ -0,0 +1,10 @@
+package com.example.devan.remedaily.Models;
+
+import com.example.devan.remedaily.datalayer.Med;
+
+import java.util.ArrayList;
+
+public class NonDailyMedicine {
+    public String DateTime;
+    public ArrayList<Med> MedicineArrayList;
+}
